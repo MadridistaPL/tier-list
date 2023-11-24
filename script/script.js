4faa12c094b0349486ba345ec229977b9c5482fd
Igor Kaczorek <143325604+MadridistaPL@users.noreply.github.com>
2023-11-24 10:52:15 +0100

    Update

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function dro(ev) {
+function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
